5bb87521d95283f7741d4228173d3a328a836bc6
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-10-27 09:39:02 -0300

    feat: first commit

---
+++ src/main/java/com/delazeri/link_bio_auth_service/LinkBioAuthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.delazeri.link_bio_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkBioAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkBioAuthServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/delazeri/link_bio_auth_service/LinkBioAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delazeri.link_bio_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkBioAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
