bde899c7bf9ac55124daae095155a12f82d158e2
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-10-29 13:02:49 -0300

    feat: creating LoginUser Request and Response

---
+++ src/main/java/com/delazeri/link_bio_auth_service/infrastructure/controllers/dtos/requests/LoginUserRequest.java
@@ -0,0 +1,4 @@
+package com.delazeri.link_bio_auth_service.infrastructure.controllers.dtos.requests;
+
+public record LoginUserRequest(String username, String password) {
+}
---
+++ src/main/java/com/delazeri/link_bio_auth_service/infrastructure/controllers/dtos/responses/LoginUserResponse.java
@@ -0,0 +1,6 @@
+package com.delazeri.link_bio_auth_service.infrastructure.controllers.dtos.responses;
+
+public record LoginUserResponse(
+        String token
+) {
+}
