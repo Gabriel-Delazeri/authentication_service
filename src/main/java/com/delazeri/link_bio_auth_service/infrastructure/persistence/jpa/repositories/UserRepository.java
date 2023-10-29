d733cd59e03866b04b1c5b14c1298c946fe373c5
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-10-29 12:08:26 -0300

    feat: create AuthorizationService implementing UserDetailsService

--- src/main/java/com/delazeri/link_bio_auth_service/infrastructure/persistence/jpa/repositories/UserRepository.java
+++ src/main/java/com/delazeri/link_bio_auth_service/infrastructure/persistence/jpa/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.delazeri.link_bio_auth_service.infrastructure.persistence.jpa.reposi
 
 import com.delazeri.link_bio_auth_service.infrastructure.persistence.jpa.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    UserDetails findByUsername(String username);
 }
